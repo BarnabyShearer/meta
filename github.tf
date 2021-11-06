1f011ec81b6a1385a53a10e2f426763656cc02d3
Barnaby Shearer <b@zi.is>
2021-11-06 14:21:44 +0000

    init

---
+++ github.tf
@@ -0,0 +1,254 @@
+locals {
+  repos_defaults = {
+    description = null
+    license  = "GPL-2.0"
+    topics   = ["hacktoberfest"]
+    archived = false
+    link     = null
+  }
+
+  repos_todo = {
+    htmloverpdf = {}
+    pgwebsocket = {}
+  }
+
+  repos_repos = {
+    meta = {
+      description = "Version control for my Github Repositories."
+    }
+    ADS124x = {
+      description = "ADS124x library for arduino."
+      topic       = ["arduino"]
+    }
+    alexa-ping = {
+      description = "Amazon Alexa skill to check website is responding."
+      link        = "https://www.amazon.co.uk/dp/b073vpbjdw/"
+    }
+    ankle = {
+      description = "A UI for visualizing a stream of location events, such as showing packages being delivered."
+      license     = "BSD-2-Clause"
+    }
+    aoc = {
+      description = "My Advent of Code."
+      link        = "https://adventofcode.com/"
+      license     = "MIT"
+      topics      = ["advent-of-code"]
+    }
+    basic_remote_shell = {
+      description = "Minimum code needed to connect to a OpenSSH Server."
+    }
+    binscript = {
+      description = "Minimal self hosting Asembler."
+    }
+    DockerFromScratch = {
+      description = "Docker From Scratch."
+      license     = "MIT"
+      topics      = ["docker"]
+    }
+    dogoban = {
+      description = "Your trusty sheepdog is hungry. Help them corral the animals needed to produce their balanced food."
+      topics      = ["game"]
+    }
+    efm8 = {
+      description = "Flash via AN945: EFM8 Factory Bootloader HID."
+      license     = "BSD-2-Clause"
+      publish     = ["pypi.org", "readthedocs.org"]
+    }
+    email-report-checker = {
+      description = "RFC 7489 & 8460 SMTP Report Monitoring Utilities."
+      license     = "BSD-3-Clause"
+    }
+    imax_b8_serial = {
+      description = "Serial interface to monitor LiPo charger."
+    }
+    kibblekhaos = {
+      description = "Your trusty space dog is hungry. You have travelled to the farm belt where the nutrient asteroids grow."
+      link        = "https://kibblekhaos.zi.is/"
+      topics      = ["game"]
+    }
+    "kvm.py" = {
+      description = "Minimalist python to run amd64 code via kvm."
+    }
+    lektor-make = {
+      description = "Lektor plugin to run `make lektor` for custom build systems."
+      license     = "MIT"
+      topics      = ["lektor"]
+      publish     = ["pypi.org", "readthedocs.org"]
+    }
+    psycopg-pool-prometheus = {
+      description = "Exposes the pool stats maintained by psycopg3's connection pool to prometheus."
+      license     = "MIT"
+      topics      = ["prometheus", "psycopg3"]
+      publish     = ["pypi.org", "readthedocs.org"]
+    }
+    pynfc = {
+      description = "`ctypeslib` converted libnfc and libfreefare."
+      license     = "BSD-2-Clause"
+      publish     = ["pypi.org", "readthedocs.org"]
+    }
+    pypg = {
+      description = "Minimal Toy PGP in pure python."
+    }
+    rov = {
+      desription = "Underwater ROV Prototype."
+    }
+    scadhelper = {
+      description = "A library to make SCAD easier."
+      topics      = ["3d-printing", "openscad", "openscad-library"]
+      license     = "CC BY 3.0"
+    }
+    things = {
+      description = "Designs for lasercutting, 3D printing, milling etc."
+      topics      = ["3d-printing"]
+      license     = null
+    }
+  }
+  repos_archived = {
+    "64bit" = {
+      description = "Firmware for https://github.com/BarnabyShearer/signalsignal"
+      link        = "https://github.com/BarnabyShearer/signalsignal"
+    }
+    Anders = {
+      description = "WIP Android app for Chinesium BLE cooking/BBQ thermometers."
+      license     = "GPL-3.0"
+      topic       = ["android", "ble"]
+    }
+    anemopen = {
+      description = "Opensource Anemometer."
+      topic       = ["arduino", "3d-printing"]
+    }
+    arm = {
+      description = "¯\\_(ツ)_/¯"
+      topic       = ["hack"]
+    }
+    async_or_not = {
+      description = "Help chose performance trade offs."
+      topic       = ["benchmark"]
+    }
+    autopi = {
+      description = "Quick and dirty script to get a headless Raspberry Pi ready to use."
+      license     = "Apache"
+    }
+    aux_lcd = {
+      description = "Ultra compact PCB using a battery, memory lcd, and bluetooth chip."
+    }
+    ble_temp_probe_decoder = {
+      description = "Bluetooth LE Cooking/BBQ temperature sensor."
+      license     = "Apache"
+    }
+    dash = {
+      description = "Amazon Dash button utils."
+    }
+    DroidRap = {
+      description = "Minimal G-Code host for Android."
+      license     = "GPL-3.0"
+      topic       = ["android"]
+    }
+    eyepatch = {
+      description = "Android camera monitor headset (phone are cheeper then eyes when using lasers)."
+      license     = "GPL-3.0"
+      topic       = ["android"]
+    }
+    falcon = {
+      description = "Demos for the Novint Falcon haptic controler."
+      license     = "BSD-2-Clause"
+    }
+    gcode2mint = {
+      description = "Allows driving the Denford MicroMill 2000 (and hopfully similar devices) via standard g-code. "
+    }
+    genuary = {
+      description = "My genuary."
+      link        = "https://genuary2021.github.io/"
+    }
+    MemoryLCD = {
+      description = "STM32F4 Sharp Memory LCD."
+      license     = "BSD-2-Clause"
+      topics      = ["arduino"]
+    }
+    microformats = {
+      description = "Micoroformats."
+      link        = "https://microformats.org/wiki/what-are-microformats"
+    }
+    microg = {
+      description = "Micro-g toy g-code interpritor designed to be highly portable; but initially on AVR."
+    }
+    nodemcu_proxy = {
+      description = "Simple UART->TCP port 6789 proxy."
+    }
+    pcb_workshop_sensor = {
+      description = "Simple lowpower MSP and Temp sensor PCB for a PCB design workshop."
+    }
+    phone_chord = {
+      description = "An 8-key chording BLE HID keyboard."
+      license     = "MIT"
+      topics      = ["arduino"]
+    }
+    PiSlideshow = {
+      description = "Updatable slideshow kiosk on Raspberry Pi."
+    }
+    ppm2joy = {
+      description = "vjoy module with decodes a 6 channel PPM signal captured via SUMP."
+      license     = "GPL-3.0"
+    }
+    pyquiz = {
+      description = "Run a simple button mashing quiz."
+      license     = "Apache"
+      topic       = ["game"]
+    }
+    rt100 = {
+      description = "UMI RT100 Robot Arm controller."
+    }
+    rtlapt = {
+      description = "NOAA Automatic Picture Transmission (Wether) reception via RTL SDR."
+    }
+    rust-minimal-wayland-egl-opengles = {
+      description = "Test rust and opgengl."
+    }
+    signalsignal = {
+      description = "PCB for cheep high-density IO."
+    }
+    slushie = {
+      description = "Smothieboard derivative."
+      license     = "CERN OHL 1.2"
+    }
+    stereo = {
+      description = "Try and get two USB cameras in sync to create stereo images."
+    }
+    terraform-provider-wait-srv = {
+      description = "Allow terraform to wait for a SRV record to exist."
+    }
+    u2f-zero = {
+      description = "U2F USB token optimized for physical security, affordability, and style."
+      license     = "BSD-2-Clause"
+
+    }
+    wavey = {
+      description = "Exeperiment on non-plainer layers on RepRap."
+      topics      = ["3d-printing"]
+    }
+    wireless_serial_camera = {
+      description = "Wireless serial camera between Arduino and RaspberryPi."
+    }
+    xarcade = {
+      description = "Simple interface to plug an X-Arcade controller in as a USB keyboard."
+      license     = "MIT"
+    }
+    xkcdris = {
+      description = "Game inspired by xkcd \"Hell\"."
+      link        = "https://xkcd.com/724/"
+      license     = "MIT"
+      topic       = ["xkcd", "tetris"]
+    }
+    yugioh = {
+      description = "Make consistent collectable cards."
+      license     = null
+    }
+    zsl = {
+      description = "Curses interface to control UVC cameras."
+    }
+  }
+  repos = { for k, v in merge(
+    { for k, v in local.repos_repos : k => merge(local.repos_defaults, v) },
+    { for k, v in local.repos_archived : k => merge(local.repos_defaults, v, { archived = true }) },
+  ) : k => merge(v, { topics = concat(local.repos_defaults.topics, v.topics) }) }
+}
---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "github_repository" "main" {
+  for_each = local.repos
+
+  name         = each.key
+  description  = each.value.description
+  homepage_url = each.value.link
+
+  has_issues   = true
+  has_projects = false
+  has_wiki     = false
+
+  allow_merge_commit = false
+
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+
+  license_template = each.value.license
+
+  #archived = each.value.archived
+
+  archive_on_destroy = true
+
+  topics = each.value.topics
+
+  vulnerability_alerts = true
+}
